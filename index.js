04014da75b08151ff55700838224f4c9f930cfc7
trangnt08 <thuytrangnguyen1306@gmail.com>
2017-03-24 00:21:32 +0700

    hello world

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ app.get('/', function (req, res) {
 	res.send('Hello world, I am a chat bot')
 })
 
+app.get('/webhooks', function (req, res) {
+  if (req.query['hub.verify_token'] === 'EAAVMi6ykpokBAFw4OidZCNeExQh47Wsda5o1HWZCDlUJGimZASpyqHIcHNUpfpMUkkEjrqPQBJw5gUXiEoj6x1ZA8nGGptZBJOT14yW6rB4BHPvZCyFdkKOKZBFUoZAyCRBKzeIREQ8QG5n4ZARHw90D512YV9fMFyazCxJChNrHNzgZDZD') {
+    res.send(req.query['hub.challenge'])
+  }
+  res.send('Error, wrong token')
+})
+
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
 	if (req.query['hub.verify_token'] === 'welcome_to_fb_nguyen_thuy_trang') {
