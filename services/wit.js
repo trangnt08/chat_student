cfa8f6301b5b94af86c6b96fed8d222b02b9629b
trangnt08 <thuytrangnguyen1306@gmail.com>
2017-03-24 16:57:03 +0700

    hello world

--- services/wit.js
+++ services/wit.js
@@ -63,12 +63,12 @@ var actions = {
 			context.subject=subject
 			context.number=number
 		}
-		// else{
-		// 	context.missingSubject = true;
-		// 	context.subject = firstEntityValue(entities,'subject')
-		// 	context.missingSubject = false;
-		// 	context.number = number
-		// }
+		else{
+			context.missingSubject = missingSubject;
+			delete context.missingSubject
+			context.subject = firstEntityValue(entities,'subject')
+			context.number = number
+		}
 
 		cb(context)
 		delete context.point
