fb0a66c0720b3879421b97aff3d2d59eeeb750e2
trangnt08 <thuytrangnguyen1306@gmail.com>
2017-03-24 11:25:54 +0700

    hello world

--- services/wit.js
+++ services/wit.js
@@ -54,11 +54,12 @@ var actions = {
 		var missingSubject = firstEntityValue(entities,'subject')
 		if (missingSubject==null || '') {
 			context.missingSubject = missingSubject
-			var subject = firstEntityValue(entities,'subject')
+		};
+		
+		var subject = firstEntityValue(entities,'subject')
 			if(subject){
 				context.subject = subject
 			}
-		};
 
 		var num = firstEntityValue(entities,'number')
 		if(num==null || ''){
